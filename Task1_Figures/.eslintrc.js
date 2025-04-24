474d66f009c8dfc55df733ea27a61cb0b15ce7e6
IvanPratasevich <iv.pratasevich@gmail.com>
2025-04-24 20:24:53 +0300

    init: project configuration setup, dependency updates and file structure adjustments

---
+++ Task1_Figures/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {},
+};
